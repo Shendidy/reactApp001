7fe12e221bdc4802f5862181997a871d2bd49b9a
Sherif Shendidy <sherif@shendidy.com>
2020-10-21 14:14:49 +0100

    routing and css completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import './Link';
 import Link from './Link';
 
 const Header = () => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+    useEffect(() => {
+        const onLocationChange = () => {setCurrentPath(window.location.pathname);}
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        }
+    }, [currentPath]);
     return (
         <div className="ui secondary pointing menu">
-            <Link href="/" className={`item${window.location.pathname === '/' ? ' bold' : ''}`}>
+            <Link href="/" className={`item${currentPath === '/' ? ' bold' : ''}`}>
                 Home
             </Link>
-            <Link href="/hello" className={`item${window.location.pathname === '/hello' ? ' bold' : ''}`}>
+            <Link href="/hello" className={`item${currentPath === '/hello' ? ' bold' : ''}`}>
                 Hello
             </Link>
-            <Link href="/about" className={`item${window.location.pathname === '/about' ? ' bold' : ''}`}>
+            <Link href="/about" className={`item${currentPath === '/about' ? ' bold' : ''}`}>
                 About
             </Link>
-            <Link href="/contact" className={`item${window.location.pathname === '/contact' ? ' bold' : ''}`}>
+            <Link href="/contact" className={`item${currentPath === '/contact' ? ' bold' : ''}`}>
                 Contact
             </Link>
         </div>
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Link = ({ className, href, children }) => {
+    const onClick = (event) => {
+        event.preventDefault();
+        window.history.pushState({}, '', href);
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+    }
     return (
-        <a href={href} className={className} >{children}</a>
+        <a onClick={onClick} href={href} className={className} >{children}</a>
     )
 };
 
