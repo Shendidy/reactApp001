d65e9a1aa21e63f4a153706223a54f15529d1be1
Sherif Shendidy <sherif@shendidy.com>
2020-10-21 10:52:52 +0100

    implementing basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import Home from './components/Home';
+import Hello from './components/Hello';
+import About from './components/About';
+import Contact from './components/Contact';
 
 const App = () => {
+    const showHome = () => {
+        if(window.location.pathname === '/') return <Home />
+    }
+
+    const showHello = () => {
+        if(window.location.pathname === '/hello') return <Hello />
+    }
+
+    const showAbout = () => {
+        if(window.location.pathname === '/about') return <About />
+    }
+
+    const showContact = () => {
+        if(window.location.pathname === '/contact') return <Contact />
+    }
+
     return (
-        <div>App</div>
+        <div>
+            {showHome()}
+            {showHello()}
+            {showAbout()}
+            {showContact()}
+        </div>
     )
 }
 
